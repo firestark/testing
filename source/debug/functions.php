c7f7c93ff91b5c6312e4d6ecd9b42c77ea499de7
Aron Wouters <lionar@mail.com>
2018-09-07 15:45:10 +0200

    Added test if dd function exists

--- source/debug/functions.php
+++ source/debug/functions.php
@@ -3,16 +3,19 @@
 use testing\debug\dumper;
 
 
-/**
- * Dump the passed variables and end the script.
- *
- * @param  mixed
- * @return void
- */
-function dd ( ...$args )
+if ( ! function_exists ( 'dd' ) )
 {
-    foreach ( $args as $x )
-        ( new Dumper )->dump ( $x );
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param  mixed
+     * @return void
+     */
+    function dd ( ...$args )
+    {
+        foreach ( $args as $x )
+            ( new Dumper )->dump ( $x );
 
-    die ( 1 );
+        die ( 1 );
+    }
 }
